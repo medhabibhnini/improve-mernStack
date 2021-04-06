530635af136e40d734247db068c0de73d60e9e5a
wifeko <49137001+wifeko@users.noreply.github.com>
2021-04-06 13:58:27 +0200

    register v1

--- client/src/components/body/auth/Register.js
+++ client/src/components/body/auth/Register.js
@@ -55,66 +55,120 @@ function Register() {
     }
 
     return (
-        <div className="login_page">
-            <div class="container">
-          <div class="header-body text-center mb-7">
-            <div class="row justify-content-center">
-              <div class="col-lg-5 col-md-6">
-                <h1 class="text-white">Welcome!</h1>
-                <p class="text-lead text-light">Use these awesome forms to login or create new account in your project for free.</p>
+        <div>
+          <div class="header bg-gradient-primary py-7 py-lg-8">
+          <div class="container">
+            <div class="header-body text-center mb-7">
+              <div class="row justify-content-center">
+                <div class="col-lg-5 col-md-6">
+                  <h1 class="text-white">Welcome!</h1>
+                  <p class="text-lead text-light">Use these awesome forms to login or create new account in your project for free.</p>
+                </div>
               </div>
             </div>
           </div>
+  
+          <div/>
+        <div/>
+  <br></br>
+  <br></br>
+  <br></br>
+        <div class="container mt--8 pb-5">
+          <div class="row justify-content-center">
+            <div class="col-lg-5 col-md-7">
+              <div class="card bg-secondary shadow border-0">
+                <div class="card-body px-lg-5 py-lg-5">
+                  <div class="text-center text-muted mb-4">
+                    <small>Please fill all the blanks</small>
+                  </div>
+                  {err && showErrMsg(err)}
+              {success && showSuccessMsg(success)}
+                  <form role="form" onSubmit={handleSubmit}>
+
+                  <div class="form-group mb-3">
+                      <div class="input-group input-group-alternative">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                        </div>
+                        <input class="form-control" placeholder="name" type="name" id="name"
+                      value={name} name="name" onChange={handleChangeInput} />
+                      </div>
+                    </div>
+
+                    
+                    <div class="form-group mb-3">
+                      <div class="input-group input-group-alternative">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                        </div>
+                        <input class="form-control" placeholder="lastName" type="lastName" id="lastName"
+                      value={lastName} name="lastName" onChange={handleChangeInput} />
+                      </div>
+                    </div>
+
+                    <div class="form-group mb-3">
+                      <div class="input-group input-group-alternative">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                        </div>
+                        <input class="form-control" placeholder="userName" type="userName" id="userName"
+                      value={userName} name="userName" onChange={handleChangeInput} />
+                      </div>
+                    </div>
+
+
+
+                    <div class="form-group mb-3">
+                      <div class="input-group input-group-alternative">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                        </div>
+                        <input class="form-control" placeholder="Email" type="email" id="email"
+                      value={email} name="email" onChange={handleChangeInput} />
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <div class="input-group input-group-alternative">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                        </div>
+                        <input class="form-control" placeholder="Password" type="password"
+                         id="password"
+                         value={password} name="password" onChange={handleChangeInput}/>
+                      </div>
+                    </div>
+
+                    <div class="form-group mb-3">
+                      <div class="input-group input-group-alternative">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                        </div>
+                        <input class="form-control" placeholder="cf_password" type="password" id="cf_password"
+                      value={cf_password} name="cf_password" onChange={handleChangeInput} />
+                      </div>
+                    </div>
+
+                    <div class="custom-control custom-control-alternative custom-checkbox">
+                      <input class="custom-control-input" id=" customCheckLogin" type="checkbox"/>
+                      <label class="custom-control-label" for=" customCheckLogin">
+                        <span class="text-muted">Remember me</span>
+                      </label>
+                    </div>
+                    <div class="text-center">
+                      <button type="button" type="submit" class="btn btn-primary my-4">Sign in</button>
+                    </div>
+                  </form>
+                  <p>Already an account? <Link to="/login">Login</Link></p>
+                </div>
+              </div>
+              
+            </div>
+          </div>
         </div>
-            <h2>Register</h2>
-            {err && showErrMsg(err)}
-            {success && showSuccessMsg(success)}
-
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" placeholder="Enter your name" id="name"
-                    value={name} name="name" onChange={handleChangeInput} />
-                </div>
-
-                <div>
-                    <label htmlFor="lastName">Last Name</label>
-                    <input type="text" placeholder="Enter your last name" id="lastName"
-                    value={lastName} name="lastName" onChange={handleChangeInput} />
-                </div>
-
-                <div>
-                    <label htmlFor="userName">User Name</label>
-                    <input type="text" placeholder="Enter your last username" id="userName"
-                    value={userName} name="userName" onChange={handleChangeInput} />
-                </div>
-
-                <div>
-                    <label htmlFor="email">Email Address</label>
-                    <input type="text" placeholder="Enter email address" id="email"
-                    value={email} name="email" onChange={handleChangeInput} />
-                </div>
-
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" placeholder="Enter password" id="password"
-                    value={password} name="password" onChange={handleChangeInput} />
-                </div>
-
-                <div>
-                    <label htmlFor="cf_password">Confirm Password</label>
-                    <input type="password" placeholder="Confirm password" id="cf_password"
-                    value={cf_password} name="cf_password" onChange={handleChangeInput} />
-                </div>
-
-                <div className="row">
-                    <button type="submit">Register</button>
-                </div>
-            </form>
-
-            <p>Already an account? <Link to="/login">Login</Link></p>
-        </div>
-    )
+      </div>
+      </div>
+      )
+    
 }
 
 export default Register
